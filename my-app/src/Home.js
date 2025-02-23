1a246ebfd180a128655bd9e4f8f6ed0b1d073a0f
Alan Lin <alanlin3008@gmail.com>
2025-02-22 20:14:51 -0500

    Add files via upload

--- my-app/src/Home.js
+++ my-app/src/Home.js
@@ -3,8 +3,6 @@ import React from 'react';
 const Home = () => {
   return (
     <div>
-      <h1>Welcome to Health Clicker</h1>
-      <p>Connect to Strava to track your activities!</p>
     </div>
   );
 };
